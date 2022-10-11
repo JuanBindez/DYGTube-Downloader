40401ab099ddce875dea818d7eb8da70cf56955e
Juan Carlos Bindez <juanbindez780@gmail.com>
2022-10-10 22:47:24 -0300

    1010222247

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-'''
-Copyright (c) 2022 Juan Carlos Bindez
-This project is licensed under the MIT License.
-'''
-
-'''
-Author: www.github.com/JuanBindez
-Description: download youtube videos
-Python Version: 3.10
-Local: Brazil
-OS: Linux
-'''
+# downloader of the Youtube.
+#
+# Copyright (c) 2022  Juan Carlos Bindez
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#  
+# autor: https://github.com/juanBindez
+# e-mail: juanbindez780@gmail.com
 
 
 try:
