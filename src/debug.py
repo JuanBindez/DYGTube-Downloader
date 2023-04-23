69f9115f6dbdddda29d61dc99e29f7eff05b2943
Juan Bindez <juanbindez780@gmail.com>
2023-04-22 22:24:09 -0300

    Update debug.py

--- src/debug.py
+++ src/debug.py
@@ -24,13 +24,15 @@
 import logging
 
 class DebugInfo:
-    logging.basicConfig(filename="DYGTUbe_Debug_info.log", level=logging.INFO,
+    logging.basicConfig(filename="DYGTUbe_Debug_info.log",
+                        level=logging.INFO,
                         format='%(asctime)s %(message)s',
                         datefmt='%d/%m/%Y %H:%M:%S')
     logger_info = logging.getLogger("DYGTUbe_Debug_info")
 
-    # Configuração para o arquivo de log de erro
-    logging.basicConfig(filename='DYGTUbe_Error.log', level=logging.ERROR,
+    logging.basicConfig(filename='DYGTUbe_Error.log',
+                        level=logging.ERROR,
                         format='%(asctime)s %(levelname)s: %(message)s',
                         datefmt='%d/%m/%Y %H:%M:%S')
+    
     logger_error = logging.getLogger("DYGTUbe_error")
